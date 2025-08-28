88e2fdd233e1f980d64ae0a0fdb91ae0354a4010
Welt <tienduy0123@gmail.com>
2025-08-28 14:56:21 +0700

    1st step to the hell

--- src-tauri/src/utils/lsp.rs
+++ src-tauri/src/utils/lsp.rs
@@ -1,14 +1,27 @@
-use std::process::{Child, Command, Stdio};
+use std::{
+    io::{BufRead, BufReader, Error},
+    process::{Command, Stdio},
+};
 
-pub fn spawn_lsp_process() -> Child {
+pub fn spawn_lsp_process(app_handle: tauri::AppHandle) -> Result<(), Error> {
     println!("pylsp starting on ws://127.0.0.1:30000");
-    Command::new("pylsp")
-        .arg("--ws")
-        .arg("--port")
-        .arg("30000")
-        .stdin(Stdio::null()) // no stdin needed for ws
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .spawn()
-        .expect("Failed to start pylsp process")
+    let mut child = Command::new("pylsp")
+        .stdin(Stdio::piped()) // no stdin needed for ws
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()?;
+
+    let stdout = child.stdout.take().unwrap();
+    let reader = BufReader::new(stdout);
+    let mut lines = reader.lines();
+
+    let app_handle_clone = app_handle.clone();
+    tokio::spawn(async move {
+        while let Some(Ok(line)) = lines.next() {
+            println!("pylsp: {}", line);
+            app_handle_clone.emit_all("lsp-log", line).unwrap();
+        }
+    });
+
+    Ok(())
 }
